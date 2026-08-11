966ee0721c9b4a11de80809af4f567c389026d7d
JAMES67676767 <jlberden@cvisc.pshs.edu.ph>
2026-08-11 14:26:47 +0800

    Add files via upload

---
+++ Berden_Jayne_DistanceProgram.py
@@ -0,0 +1,12 @@
+import math
+x1 = int(input("Enter your x1: "))
+x2 =int(input("Enter your x2: "))
+y1 =int(input("Enter your y1: "))
+y2 = int(input("Enter your y2: "))
+
+distance = math.sqrt((x2 - x1) **2 + (y2 - y1) **2)
+print (f"Your distance is {distance:.2F}")
+
+#The math library helped simplify it by making things alot quicker and easier. While I was coding, it felt like a breeze because of the library. 
+#The functions that were easier to use are finding the square root and alot more. It overall made things alot easier.
+#The program would be more difficult because you would have to repeat stuff all over again. Because of this, the math library madde things easier.
